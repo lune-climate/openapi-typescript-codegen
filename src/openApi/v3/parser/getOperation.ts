64e74b7b8a4065e738794efac582ffa770e6c126
Ruben Aguiar <r.aguiar9080@gmail.com>
2022-06-30 14:13:32 +0100

    Rework methods interface
    After some debate, we have decide to rework how we present the interface
    for service methods. Two main changes are present here:
    - Add option to override the account to be used on the endpoint for a
      single operation. This is made with a `options` object that is
    optional and currently contains an `accountId` field. If we have more
    to add in the future, the object structure allows us to easily do it.
    - Move both queryParams and bodyParams to the same object. This makes it
      a lot easier for our clients to use and doesn't leak details about the
    underlying API structure. This does require the parameters to not share
    any of the property names, but this is already the case and we prefer
    the tradeoff. This hasn't been fully done on the v2 openApi schema since
    bodyParams are not present at the same level, but since we use v3 this
    doesn't affect us.
    
    To note that the new `data` object that holds query and body parameters
    is not nullable and it is optional if all parameters contained in it are
    optional. The parameters inside `data` have also been sorted by required
    status, mimicking the behaviour that existed for when they were outside.
    
    An example of a possible interface with parameters in all places
    (descriptions and comments are ommited):
    ```typescript
    public methodX(
        queryParam1: string,
        queryParam2: string,
        data: {
            bodyParam1: typeX,
            queryParam2: number,
            bodyParam2?: typeZ,
            queryParam1?: string,
        },
        options?: { accountId: string },
    )
    ```
    
    Signed-off-by: Ruben Aguiar <r.aguiar9080@gmail.com>
    
    qwe
    
    Signed-off-by: Ruben Aguiar <r.aguiar9080@gmail.com>

--- src/openApi/v3/parser/getOperation.ts
+++ src/openApi/v3/parser/getOperation.ts
@@ -1,3 +1,4 @@
+import { Model } from '../../../client/interfaces/Model';
 import type { Operation } from '../../../client/interfaces/Operation';
 import { OperationParameter } from '../../../client/interfaces/OperationParameter';
 import type { OperationParameters } from '../../../client/interfaces/OperationParameters';
@@ -13,7 +14,7 @@ import { getOperationResponses } from './getOperationResponses';
 import { getOperationResults } from './getOperationResults';
 import { getRef } from './getRef';
 import { getServiceName } from './getServiceName';
-import { sortByRequired } from './sortByRequired';
+import { sortByRequired, sortModelByRequired } from './sortByRequired';
 
 export const getOperation = (
     openApi: OpenApi,
@@ -48,38 +49,43 @@ export const getOperation = (
         responseHeader: null,
     };
 
+    // We want to treat query parameters and body request as a single object with all the parameters
+    // inside. This allows for better and more concise usage (named parameters) for our clients. We are
+    // aware this makes names between query an request parameters cannot be duplicate, which is a
+    // possibility. However, we currently don't have this case and agreed on avoiding in the future
+    // for the sake of improving our clients interface. Extract the query params and the request body
+    // (if it exists) and join them accordingly in a new dataParameter object.
+    const dataParameter: OperationParameter = {
+        // value here is actually ignore, we just need to provide a valid one
+        in: 'query',
+        export: 'interface',
+        prop: 'data',
+        name: 'data',
+        type: 'any',
+        base: 'any',
+        template: null,
+        link: null,
+        description: null,
+        default: undefined,
+        isDefinition: false,
+        isReadOnly: false,
+        isRequired: false,
+        isNullable: false,
+        imports: [],
+        enum: [],
+        enums: [],
+        properties: [],
+        mediaType: null,
+    };
+
     // Parse the operation parameters (path, query, body, etc).
     if (op.parameters) {
         const parameters = getOperationParameters(openApi, op.parameters);
 
-        // We want to treat query parameters as a single object with all the parameters inside. This
-        // allows for better and more concise usage (named parameters). For that, extract them from
-        // the parameters, create a fake operation for them and addem them individually as single object
         const queryParams = parameters.parameters.filter(p => p.in === 'query');
         if (queryParams.length !== 0) {
-            const queryParamsObject: OperationParameter = {
-                in: 'query',
-                export: 'interface',
-                prop: 'queryParams',
-                name: 'queryParams',
-                type: 'any',
-                base: 'any',
-                template: null,
-                link: null,
-                description: null,
-                default: undefined,
-                isDefinition: false,
-                isReadOnly: false,
-                isRequired: !!queryParams.find(p => p.isRequired),
-                isNullable: !!!queryParams.find(p => !p.isNullable),
-                imports: [],
-                enum: [],
-                enums: [],
-                properties: queryParams,
-                mediaType: null,
-            };
-
-            operation.parameters.push(queryParamsObject);
+            dataParameter.properties.push(...queryParams);
+            dataParameter.isRequired = !!queryParams.find(p => p.isRequired);
         }
 
         const newParams = parameters.parameters.filter(p => p.in !== 'query');
@@ -97,8 +103,9 @@ export const getOperation = (
         const requestBodyDef = getRef<OpenApiRequestBody>(openApi, op.requestBody);
         const requestBody = getOperationRequestBody(openApi, requestBodyDef);
         operation.imports.push(...requestBody.imports);
-        operation.parameters.push(requestBody);
         operation.parametersBody = requestBody;
+        dataParameter.properties.push(...requestBody.properties);
+        dataParameter.isRequired = requestBody.isRequired ? true : dataParameter.isRequired;
     }
 
     // Parse the operation responses.
@@ -114,7 +121,54 @@ export const getOperation = (
         });
     }
 
+    if (dataParameter.properties.length !== 0) {
+        operation.parameters.push(dataParameter);
+        dataParameter.properties = dataParameter.properties.sort(sortModelByRequired);
+    }
     operation.parameters = operation.parameters.sort(sortByRequired);
+
+    // Add an options object parameter for options on the methods themselves. Currently allow an account
+    // override that can use `Lune-Account` header to override target account on a per endpoint basis.
+    operation.parameters.push({
+        in: 'query',
+        export: 'interface',
+        prop: 'options',
+        name: 'options',
+        type: 'any',
+        base: 'any',
+        template: null,
+        link: null,
+        description: 'Additional operation options',
+        default: undefined,
+        isDefinition: false,
+        isReadOnly: false,
+        isRequired: false,
+        isNullable: false,
+        imports: [],
+        enum: [],
+        enums: [],
+        properties: [
+            {
+                export: 'generic',
+                name: 'accountId',
+                type: 'string',
+                base: 'string',
+                template: null,
+                link: null,
+                description: 'Account Id to be used to perform the API call',
+                default: undefined,
+                isDefinition: false,
+                isReadOnly: false,
+                isRequired: false,
+                isNullable: false,
+                imports: [],
+                enum: [],
+                enums: [],
+                properties: [],
+            } as Model,
+        ],
+        mediaType: null,
+    });
 
     return operation;
 };
