fb400832ae77763aa0e4ff23f515113b6eb66dbf
Rúben Aguiar <r.aguiar9080@gmail.com>
2022-07-28 11:45:21 +0100

    Allow empty string as enum entry (#35)
    This is a usecase we have in our OpenAPI schema. To allow it, the enum
    is allowed to pass in the parsing stage, and the template deals with this
    special case by naming the enum the empty string ('').
    
    A new model was added to the tests that triggers the new code paths to
    make sure generation is working.
    
    Signed-off-by: Ruben Aguiar <r.aguiar9080@gmail.com>

--- src/openApi/v3/parser/getEnum.ts
+++ src/openApi/v3/parser/getEnum.ts
@@ -1,5 +1,4 @@
 import type { Enum } from '../../../client/interfaces/Enum';
-import { isDefined } from '../../../utils/isDefined';
 
 export const getEnum = (values?: (string | number)[]): Enum[] => {
     if (Array.isArray(values)) {
@@ -7,7 +6,6 @@ export const getEnum = (values?: (string | number)[]): Enum[] => {
             .filter((value, index, arr) => {
                 return arr.indexOf(value) === index;
             })
-            .filter(isDefined)
             .map(value => {
                 if (typeof value === 'number') {
                     return {
@@ -18,11 +16,13 @@ export const getEnum = (values?: (string | number)[]): Enum[] => {
                     };
                 }
                 return {
-                    name: String(value)
-                        .replace(/\W+/g, '_')
-                        .replace(/^(\d+)/g, '_$1')
-                        .replace(/([a-z])([A-Z]+)/g, '$1_$2')
-                        .toUpperCase(),
+                    name: value
+                        ? String(value)
+                              .replace(/\W+/g, '_')
+                              .replace(/^(\d+)/g, '_$1')
+                              .replace(/([a-z])([A-Z]+)/g, '$1_$2')
+                              .toUpperCase()
+                        : '__EMPTY__',
                     value: `'${value.replace(/'/g, "\\'")}'`,
                     type: 'string',
                     description: null,
